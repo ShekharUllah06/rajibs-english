98168cdcbacd5955403936395f706b7d49b73de7
Shekhar <aa1912275@gmail.com>
2023-10-28 13:05:35 +0600

    admin: create master template

---
+++ admin/newcourse.php
@@ -0,0 +1,169 @@
+<div class="row">
+  <div class="col">
+    <p class="text-white mt-5 mb-5"></p>
+  </div>
+</div>
+<div class="row tm-content-row">
+  <div class="col-sm-12 col-md-12 col-lg-8 col-xl-8 tm-block-col">
+    <div class="tm-bg-primary-dark tm-block tm-block-h-auto">
+      <div class="row">
+        <div class="col-12">
+          <h2 class="tm-block-title d-inline-block">New Course</h2>
+        </div>
+      </div>
+      <div class="row tm-edit-product-row">
+        <div class="col-xl-6 col-lg-6 col-md-12">
+          <form action="" method="post" class="tm-edit-product-form">
+            <div class="form-group mb-3">
+              <label for="name">Course Name
+              </label>
+              <input id="name" name="name" type="text" value="Lorem Ipsum Product" class="form-control validate" />
+            </div>
+            <div class="form-group mb-3">
+              <label for="description">Description</label>
+              <textarea class="form-control validate tm-small" rows="5" required>Lorem ipsum dolor amet gentrify glossier locavore messenger bag chillwave hashtag irony migas wolf kale chips small batch kogi direct trade shaman.</textarea>
+            </div>
+            <div class="form-group mb-3">
+              <label for="category">Category</label>
+              <select class="custom-select tm-select-accounts" id="category">
+                <option>Select category</option>
+                <option value="1" selected>New Arrival</option>
+                <option value="2">Most Popular</option>
+                <option value="3">Trending</option>
+              </select>
+            </div>
+            <div class="row">
+              <div class="form-group mb-3 col-xs-12 col-sm-6">
+                <label for="expire_date">Expire Date
+                </label>
+                <input id="expire_date" name="expire_date" type="text" value="22 Oct, 2020" class="form-control validate" data-large-mode="true" />
+              </div>
+              <div class="form-group mb-3 col-xs-12 col-sm-6">
+                <label for="stock">Price
+                </label>
+                <input id="stock" name="stock" type="text" value="19,765" class="form-control validate" />
+              </div>
+            </div>
+
+        </div>
+        <div class="col-xl-6 col-lg-6 col-md-12 mx-auto mb-4">
+          <div class="tm-product-img-edit mx-auto">
+            <img src="img/product-image.jpg" alt="Product image" class="img-fluid d-block mx-auto">
+            <i class="fas fa-cloud-upload-alt tm-upload-icon" onclick="document.getElementById('fileInput').click();"></i>
+          </div>
+          <div class="custom-file mt-3 mb-3">
+            <input id="fileInput" type="file" style="display:none;" />
+            <input type="button" class="btn btn-primary btn-block mx-auto" value="CHANGE IMAGE NOW" onclick="document.getElementById('fileInput').click();" />
+          </div>
+        </div>
+        <div class="col-12">
+          <button type="submit" class="btn btn-primary btn-block text-uppercase">Update Now</button>
+        </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4 tm-block-col">
+    <div class="tm-bg-primary-dark tm-block tm-block-product-categories">
+      <h2 class="tm-block-title">Product Categories</h2>
+      <div class="tm-product-table-container">
+        <table class="table tm-table-small tm-product-table">
+          <tbody>
+            <tr>
+              <td class="tm-product-name">Product Category 1</td>
+              <td class="text-center">
+                <a href="#" class="tm-product-delete-link">
+                  <i class="far fa-trash-alt tm-product-delete-icon"></i>
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td class="tm-product-name">Product Category 2</td>
+              <td class="text-center">
+                <a href="#" class="tm-product-delete-link">
+                  <i class="far fa-trash-alt tm-product-delete-icon"></i>
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td class="tm-product-name">Product Category 3</td>
+              <td class="text-center">
+                <a href="#" class="tm-product-delete-link">
+                  <i class="far fa-trash-alt tm-product-delete-icon"></i>
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td class="tm-product-name">Product Category 4</td>
+              <td class="text-center">
+                <a href="#" class="tm-product-delete-link">
+                  <i class="far fa-trash-alt tm-product-delete-icon"></i>
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td class="tm-product-name">Product Category 5</td>
+              <td class="text-center">
+                <a href="#" class="tm-product-delete-link">
+                  <i class="far fa-trash-alt tm-product-delete-icon"></i>
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td class="tm-product-name">Product Category 6</td>
+              <td class="text-center">
+                <a href="#" class="tm-product-delete-link">
+                  <i class="far fa-trash-alt tm-product-delete-icon"></i>
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td class="tm-product-name">Product Category 7</td>
+              <td class="text-center">
+                <a href="#" class="tm-product-delete-link">
+                  <i class="far fa-trash-alt tm-product-delete-icon"></i>
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td class="tm-product-name">Product Category 8</td>
+              <td class="text-center">
+                <a href="#" class="tm-product-delete-link">
+                  <i class="far fa-trash-alt tm-product-delete-icon"></i>
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td class="tm-product-name">Product Category 9</td>
+              <td class="text-center">
+                <a href="#" class="tm-product-delete-link">
+                  <i class="far fa-trash-alt tm-product-delete-icon"></i>
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td class="tm-product-name">Product Category 10</td>
+              <td class="text-center">
+                <a href="#" class="tm-product-delete-link">
+                  <i class="far fa-trash-alt tm-product-delete-icon"></i>
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td class="tm-product-name">Product Category 11</td>
+              <td class="text-center">
+                <a href="#" class="tm-product-delete-link">
+                  <i class="far fa-trash-alt tm-product-delete-icon"></i>
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <!-- table container -->
+      <button class="btn btn-primary btn-block text-uppercase mb-3">
+        Add new category
+      </button>
+    </div>
+  </div>
+</div>

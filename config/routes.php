36a3f00e299f95db79c99b795e07650b70bbb101
Shekhar <aa1912275@gmail.com>
2023-12-11 13:00:56 +0600

    restructure framework

--- config/routes.php
+++ config/routes.php
@@ -2,11 +2,11 @@
 
 return [
   'GET' => [
-    '/rajibs-english/public/admin/login' => 'App\Admin\Controller\AdminController@login',
+    '{baseURL}/admin/login' => 'App\Admin\Controller\AdminController@login',
   ],
 
   'POST' => [
-    '/login' => 'App\Auth\Controller\AuthController@login',
-    '/logout' => 'App\Auth\Controller\AuthController@logout',
+    '{baseURL}/login' => 'App\Auth\Controller\AuthController@login',
+    '{baseURL}/logout' => 'App\Auth\Controller\AuthController@logout',
   ],
 ];
--- index.php
+++ index.php
@@ -2,17 +2,20 @@
 
 use App\Core\Application;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 // Initialize the application
+// Define the base URL
+$baseURL = '/rajibs-english';
 $app = new Application(dirname(__DIR__));
 
 // Load routes
 $router = $app->getRouter();
-$routes = include __DIR__ . '/../config/routes.php';
+$routes = include __DIR__ . '/config/routes.php';
 
 foreach ($routes as $method => $routeList) {
   foreach ($routeList as $uri => $handler) {
+    $uri = str_replace('{baseURL}', $baseURL, $uri);
     $router->$method($uri, $handler);
   }
 }

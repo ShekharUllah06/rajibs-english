8e7f369c543d89b98ea158962f2e761e46a75eff
Shekhar <aa1912275@gmail.com>
2023-12-15 21:53:53 +0600

    indent correction

--- app/Admin/Controller/AdminController.php
+++ app/Admin/Controller/AdminController.php
@@ -1,18 +1,19 @@
 <?php
+
 namespace App\Admin\Controller;
 
 use App\Core\View;
 
 class AdminController
 {
-public function login()
-{
-View::render('Admin', 'login', ['username' => 'John']);
-}
+  public function login()
+  {
+    View::render('Admin', 'login', ['username' => 'John']);
+  }
 
-public function dashboard()
-{
-$content = View::renderPartial('Admin', 'dashboard');
-return View::render('admin','index', ['content' => $content]);
-}
+  public function dashboard()
+  {
+    $content = View::renderPartial('Admin', 'dashboard');
+    return View::render('admin', 'index', ['content' => $content]);
+  }
 }
